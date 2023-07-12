d334862f829b8213a4098f7dced791722f5274ea
Andrew Lee <alee14@bu.edu>
2023-07-11 20:59:57 -0400

    working on debugging categories retrieval from view

--- backend/cards/urls.py
+++ backend/cards/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("categories/", views.categories, name="categories"),
+    path("terms_by_category/<str:category_name>/", views.terms_by_category, name="terms_by_category"),
 ]
--- backend/cards/views.py
+++ backend/cards/views.py
@@ -4,13 +4,9 @@ from django.shortcuts import get_object_or_404
 from .models import Term, Category
 
 def index(request):
-    # Retrieve all terms and verbs from the database
     terms = Term.objects.all()
-
-    # Serialize terms and verbs into JSON format
     terms_data = [{'id': term.id, 'word': term.word, 'definition': term.definition} for term in terms]
 
-    # Create a JSON response with the serialized data
     data = {
         'terms': terms_data,
     }
@@ -18,6 +14,21 @@ def index(request):
     return JsonResponse(data)
 
 def categories(request):
-    beginner_category = Category.objects.get(name='beginner')
-    intermediate_category = Category.objects.get(name='intermediate')
-    advanced_category = Category.objects.get(name='advanced')
+    category_names = Category.objects.values_list('name', flat=True).distinct()
+    data = {
+        'categories': list(category_names),
+    }
+
+    return JsonResponse(data)
+
+def terms_by_category(request, category_name):
+    category = get_object_or_404(Category, name=category_name)
+
+    terms = category.terms.all()
+    terms_data = [{'id': term.id, 'word': term.word, 'definition': term.definition} for term in terms]
+
+    data = {
+        'terms': terms_data,
+    }
+
+    return JsonResponse(data)
